76cde0204d5f8b2b7fa79fad8c50bd9929a155d1
Thomas Edvalson <machin3@gmail.com>
2014-04-10 23:55:10 -0400

    Add foundation build env. with basic layout.

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: [
+          'bower_components/foundation/scss',
+          'bower_components/foundation-icon-fonts'
+        ]
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'css/app.css': 'scss/app.scss'
+        }        
+      }
+    },
+
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'scss',
+          cssDir: 'css',
+          environment: 'production'
+        }
+      }
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      },
+      usemin: {
+        files: ['js/**/*.js', 'index.html'],
+        tasks: ['copy','useminPrepare','concat','uglify','usemin']
+      }
+    },
+
+    copy: {
+      index: {
+        src: 'index_build.html',
+        dest: 'index.html',
+      },
+    },
+
+    useminPrepare: {
+      html: 'index_build.html',
+      options: {
+        dest: '.'
+      }
+    },
+    usemin: {
+      html: 'index.html',
+      options: {
+        assetsDirs: ['foo/bar', 'bar']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-usemin');
+
+  grunt.registerTask('build', ['sass','copy','useminPrepare','concat','uglify','usemin']);
+  grunt.registerTask('default', ['build','watch']);
+}
---
+++ js/app.js
@@ -0,0 +1,36 @@
+$(document).foundation({
+	magellan:{
+		// specify the class used for active sections
+		active_class: 'active',
+		// how many pixels until the magellan bar sticks, 0 = auto
+		threshold: 0,
+		// pixels from the top of destination for it to be considered active
+		destination_threshold: 20,
+		// calculation throttling to increase framerate
+		throttle_delay: 50 
+	}
+});
+
+$(function(){
+	$('path').each(function(){
+		var $this = $(this),
+		    color = $this.css('fill'),
+		    strokelength = $this[0].getTotalLength(),
+		    strokewidth = $this.css('stroke-width');
+
+		$this.css({
+			"stroke": color,
+			"stroke-dasharray": strokelength,
+			"stroke-dashoffset": strokelength,
+			"stroke-linecap": 'round',
+			"fill-opacity": 0,
+			"stroke-linejoin": 'round'
+		});
+		$this.attr({
+			'data-start': 'stroke-dashoffset:'+strokelength+';',
+			'data-200-end': 'stroke-dashoffset:0; fill-opacity:0; stroke-width:'+strokewidth+'; stroke-opacity:1;',
+			'data-end': 'fill-opacity:1; stroke-width:0; stroke-opacity:0;'
+		});
+	});
+	var s = skrollr.init();
+});
